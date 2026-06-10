c9a1a152eaf8261ea7920e5958b7f61da6b8898e
Federico Heinrich <fedeheinrich199@gmail.com>
2026-06-10 19:30:08 -0300

    feat(generos): agrega controlador y rutas para géneros

---
+++ backend/controllers/generoController.js
@@ -0,0 +1,8 @@
+// ==========================================
+// 1. BASE DE DATOS REAL (Descomentar después)
+// ==========================================
+// const { Genero } = require('../models');
+
+
+
+module.exports = {}
