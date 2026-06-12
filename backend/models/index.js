d154d77a2b15c8403b5396a8160a48297bee9b13
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-06-12 18:03:22 +0000

    Se agregan los nuevos modelos y relaciones

--- backend/models/index.js
+++ backend/models/index.js
@@ -22,8 +22,62 @@ const sequelize = new Sequelize(
 const UserModel = require('./User');
 const User = UserModel(sequelize);
 
+const JuegoModel = require('./Juego');
+const Juego = JuegoModel(sequelize);
+
+const JuegoUserModel = require('./JuegoUser');
+const JuegoUser = JuegoUserModel(sequelize);
+
+const JuegoGeneroModel = require('./JuegoGenero');
+const JuegoGenero = JuegoGeneroModel(sequelize);
+
+const GeneroModel = require('./Genero');
+const Genero = GeneroModel(sequelize);
+
+const JuegoPlataformaModel = require('./JuegoPlataforma');
+const JuegoPlataforma = JuegoPlataformaModel(sequelize);
+
+const PlataformaModel = require('./Plataforma');
+const Plataforma = PlataformaModel(sequelize);
+
+Juego.belongsToMany(User, {
+  through: JuegoUser,
+  foreignKey: 'id_juego'
+});
+
+User.belongsToMany(Juego, {
+  through: JuegoUser,
+  foreignKey: 'id_usuario'
+});
+
+Juego.belongsToMany(Genero, {
+  through: JuegoGenero,
+  foreignKey: 'id_juego'
+});
+
+Genero.belongsToMany(Juego, {
+  through: JuegoGenero,
+  foreignKey: 'id_genero'
+});
+
+Juego.belongsToMany(Plataforma, {
+  through: JuegoPlataforma,
+  foreignKey: 'id_juego'
+});
+
+Plataforma.belongsToMany(Juego, {
+  through: JuegoPlataforma,
+  foreignKey: 'id_plataforma'
+});
+
 module.exports = {
   sequelize,
   Sequelize,
-  User
+  User,
+  Juego,
+  JuegoUser,
+  JuegoPlataforma,
+  Plataforma,
+  JuegoGenero,
+  Genero
 };
