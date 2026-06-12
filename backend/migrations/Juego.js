52f38d250fedfc6a63d33956107710428c3041a5
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-06-12 19:32:15 +0000

    Se agrega el archivo de migracion de juegos

---
+++ backend/migrations/Juego.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('juegos', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      calificacion: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      lanzamiento: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      url_imagen: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('juegos')
+    }
+  }
