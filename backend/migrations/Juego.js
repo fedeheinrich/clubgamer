8d51f0b60c34725c6e3e1b2978b91f14a684c183
Federico Heinrich <64234014+fedeheinrich@users.noreply.github.com>
2026-06-15 13:41:44 -0300

    fix(juegos): sincronizar columnas de la migracion con el modelo definitivo

--- backend/migrations/Juego.js
+++ backend/migrations/Juego.js
@@ -9,15 +9,16 @@ module.exports = {
       },
       id_rawg: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: true, // Corregido: El administrador puede crear un juego que no exista en la API externa
+        unique: true   // Agrega restriccion de unicidad para evitar guardar dos veces el mismo juego de RAWG.
       },
       titulo: {
         type: Sequelize.STRING,
         allowNull: false
       },
-      calificacion: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
+      calificacion_global: { // Corregido el nombre, ya que se cambio a calificacion_global para mayor claridad
+        type: Sequelize.FLOAT, // Corregido a FLOAT ya que RAWG lo devuelve en ese formato
+        allowNull: true,     // Corregido a true por si el administrador crea un juego que no existe en la API externa y no desea ponerle calificacion (por deberse a un estreno que aun no tiene calificacion,etc)
         validate: {
           min: 0,
           max: 5
