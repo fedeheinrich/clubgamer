8cd0555351c315d3e8ee3bdb138d8156055aa169
Federico Heinrich <64234014+fedeheinrich@users.noreply.github.com>
2026-06-15 14:14:27 -0300

    fix(migrations): corrige orden de parametros, palabra del enum, nombres identicos al modelo y activa clave compuesta

--- backend/migrations/JuegoUser.js
+++ backend/migrations/JuegoUser.js
@@ -1,41 +1,42 @@
+// backend/migrations/JuegoUser.js
 module.exports = {
-  async up(Sequelize, queryInterface) {
-    await queryInterface.createTable('juegos_users', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('juegos_usuarios', { // Corregido al nombre correcto
       id_juego: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Clave primaria compuesta parte 1
         references: {
         model: 'juegos',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE', // Se agrega para limpiar tabla intermedia en caso de que se elimine un juego
+        onDelete: 'CASCADE'
       },
       id_usuario: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Clave primaria compuesta parte 2
         references: {
         model: 'users',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE', // Se agrega para limpiar tabla intermedia en caso de que se elimine un usuario
+        onDelete: 'CASCADE'
       },
       estado: {
-        type: Sequelize.ENUM('completado', 'jugado', 'pendiente'),
+        type: Sequelize.ENUM('completado', 'jugando', 'pendiente'), // Corregido, le faltaba la n
         allowNull: false,
         defaultValue: 'pendiente'
       },
       calificacion_personal: {
         type: Sequelize.INTEGER,
-        allowNull: true,
-        validate: {
-            max: 5,
-            min: 0
-        }
+        allowNull: true     // Corregido: se saco la validacion ya que las validaciones de escala (1-5) actúan directo en el modelo
       },
-      tiempo_de_juego: {
+      tiempo_de_juego_horas: { //Corregido al nombre que se usa en el modelo
         type: Sequelize.FLOAT,
-        allowNull: false,
-        validate: {
-            min: 0
-        }
+        defaultValue: 0, // Agrega el valor inicial
+        allowNull: false // Corregido, las validaciones se realizan en el modelo
       },
       createdAt: {
         type: Sequelize.DATE,
@@ -48,6 +49,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('juegos_users')
+    await queryInterface.dropTable('juegos_usuarios') // Corregido el nombre
   }
 }
