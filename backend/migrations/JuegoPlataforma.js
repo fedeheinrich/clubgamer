be07dc3244c8c28f8a7a7d207aac04fc4fce0c58
Federico Heinrich <64234014+fedeheinrich@users.noreply.github.com>
2026-06-15 14:00:22 -0300

    fix(migrations): corrige orden de los parametros para que no crashee y activa la clave compuesta

--- backend/migrations/JuegoPlataforma.js
+++ backend/migrations/JuegoPlataforma.js
@@ -1,21 +1,28 @@
+// backend/migrations/JuegoPlataforma.js
 module.exports = {
-  async up(Sequelize, queryInterface) {
+  async up(queryInterface, Sequelize) { // Corregi el orden de los parametros
     await queryInterface.createTable('juegos_plataformas', {
       id_juego: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Corregido: Clave Compuesta parte 1
         references: {
         model: 'juegos',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE' // Si se elimina un juego, limpia sus plataformas en la intermedia
       },
       id_plataforma: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Corregido: Clave Compuesta parte 2
         references: {
         model: 'plataformas',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE' // Si se elimina una plataforma, limpia los juegos asociados en la intermedia
       },
       createdAt: {
         type: Sequelize.DATE,
