25d8d04cf3d0adba99ee3c486fbcdd437a6b0e19
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-06-15 14:51:18 +0000

    Se agrega la migración género

---
+++ backend/migrations/Genero.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('generos', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('generos')
+  }
+}
