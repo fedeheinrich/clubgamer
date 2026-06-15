801ebdad4ad010d714881b8a1ea103ec0808a91e
Federico Heinrich <64234014+fedeheinrich@users.noreply.github.com>
2026-06-15 13:53:16 -0300

    fix(juego-genero): fix(juego-genero): corrige sintaxis de la migracion y estructura clave compuesta

--- backend/migrations/JuegoGenero.js
+++ backend/migrations/JuegoGenero.js
@@ -1,21 +1,28 @@
+// backend/migrations/JuegoGenero.js
 module.exports = {
-  async up(Sequelize, queryInterface) {
+  async up(queryInterface, Sequelize) { // Corregí el orden
     await queryInterface.createTable('juegos_generos', {
       id_juego: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Corregido: Clave compuesta parte 1 (Igual al modelo)
         references: {
         model: 'juegos',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',  // Se agrego para que si se actualiza/borra un juego se limpie la tabla intermedia
+        onDelete: 'CASCADE'   
       },
       id_genero: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        primaryKey: true, // Corregido: Clave Compuesta parte 2 (Igual al modelo)
         references: {
         model: 'generos',
         key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE', // Si actualizan/borran un genero, limpia la intermedia
+        onDelete: 'CASCADE' 
       },
       createdAt: {
         type: Sequelize.DATE,
