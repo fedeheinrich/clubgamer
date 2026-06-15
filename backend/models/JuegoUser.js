979e5c8ffaf5612c10656a71076bed3022aa11b8
Federico Heinrich <64234014+fedeheinrich@users.noreply.github.com>
2026-06-15 01:25:13 -0300

    fix(juegoUser): configurar clave compuesta, ajustar enums y corrección de valores iniciales

--- backend/models/JuegoUser.js
+++ backend/models/JuegoUser.js
@@ -4,15 +4,17 @@ module.exports = (sequelize) => {
   const JuegoUser = sequelize.define('JuegoUser', {
     id_juego: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true // Clave Compuesta Parte 1
     },
     id_usuario: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true // Clave Compuesta Parte 2
     },
     calificacion_personal: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true, // Corregido a true para que inicie en null, un usuario que agrego un juego en estado "Pendiente" no deberia poder calificarlo
       validate: {
         min: 1,
         max: 10
@@ -20,18 +22,20 @@ module.exports = (sequelize) => {
     },
     tiempo_de_juego_horas: {
       type: DataTypes.FLOAT,
+      defaultValue: 0, // Corregido: inicia en 0 horas
       allowNull: false
     },
     estado: {
-        type: DataTypes.ENUM(
-            'Completado',
-            'Jugando',
-            'Pendiente'
+        type: DataTypes.ENUM( // Correcion de sintaxis, ENUM todo Mayuscula o todo Minuscula
+            'completado',
+            'jugando',
+            'pendiente'
         ),
-        allowNull: false
+        allowNull: false,
+        defaultValue: 'pendiente'
     }
   }, {
-    tableName: 'juegoUser',
+    tableName: 'juegos_usuarios', // Corrige a plural para mantener consistencia con los demas archivos
     timestamps: true,
   });
 
