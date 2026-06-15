fefab61607b9e1bdd48ab954572bc496bbcd2bb7
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-06-15 14:52:45 +0000

    Se agrega la migración para la tabla puente juego_plataforma

---
+++ backend/migrations/JuegoPlataforma.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async up(Sequelize, queryInterface) {
+    await queryInterface.createTable('juego_plataforma', {
+      id_juego: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+        model: 'juegos',
+        key: 'id'
+        }
+      },
+      id_plataforma: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+        model: 'plataformas',
+        key: 'id'
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('juego_plataforma')
+  }
+}
