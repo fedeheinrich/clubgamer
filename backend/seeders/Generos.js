dec14147f012f670da76723cc5123b33171751fa
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-06-16 19:48:04 +0000

    Se crean los seeders de la tabla géneros consultando a la api

---
+++ backend/seeders/Generos.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const response = await axios.get(
+            `https://api.rawg.io/API/genres?key=${process.env.RAWG_API_KEY}`
+        )
+
+        const generos = response.data.map(genero => ({
+            id_rawg: genero.results.id,
+            nombre: genero.results.name,
+            slug: genero.results.slug,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }));
+
+        await queryInterface.bulkInsert(
+            'generos',
+            generos
+        );
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete(
+            'generos',
+            null,
+            {}
+        );
+    }
+}
