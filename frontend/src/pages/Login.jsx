48288519555281b214b01800fd7439ed048f5fda
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-07-15 16:42:01 +0000

    Corrección de conflictos

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,57 @@
-import PlaceholderPage from '../components/common/PlaceholderPage';
+import { useState } from "react";
 
 function Login() {
-  return (
-    <PlaceholderPage
-      title="Ingreso de usuarios"
-      description="Esta ruta queda preparada para el formulario de login, la validación de credenciales y el manejo de sesión."
-      backLabel="Volver a la bienvenida"
-    />
-  );
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function enviar(event) {
+        event.preventDefault();
+        await enviarCredenciales(email, password);
+    }
+
+    return (
+        <div>
+            <h1>Iniciar sesión</h1>
+
+            <form onSubmit={enviar}>
+                <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button type="submit">Ingresar</button>
+            </form>
+        </div>
+    )
+}
+
+async function enviarCredenciales(email, password) {
+    try {
+        const respuesta = await fetch(process.env.REACT_APP_API_URL/*import.meta.env.VITE_API_URL*/, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+
+                email,
+
+                password
+
+            })
+
+        });
+        const datos = await respuesta.json();
+        if (respuesta.ok) {
+            localStorage.setItem("token", datos.token)
+        }
+        else {
+            alert('Login inválido')
+        }
+    }
+    catch(error) {
+        alert(error);
+    }
 }
 
 export default Login;
