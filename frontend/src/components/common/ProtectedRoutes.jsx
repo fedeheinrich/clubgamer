1e98f941339639af1f30d4bdf5d76ae7b1a9254e
Oviedo-Matias <matiasnahuel2023a@gmail.com>
2026-07-23 14:57:05 +0000

    Se actualiza ProtectedRoute para añadir replace

--- frontend/src/components/common/ProtectedRoutes.jsx
+++ frontend/src/components/common/ProtectedRoutes.jsx
@@ -3,7 +3,7 @@ import useAuth from "../../hooks/useAuth";
 
 function ProtectedRoute({ children }) {
     const { isAuthenticated } = useAuth();
-    return isAuthenticated ? children : <Navigate to="/login" />;
+    return isAuthenticated ? children : <Navigate to="/login" replace />;
 }
 
 export default ProtectedRoute;
